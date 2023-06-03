ba7443342e8c92a7270795b69b2ac4a3833cd035
sanju1831 <msanju2670@gmail.com>
2023-06-03 11:33:46 +0530

    calculator

---
+++ src/com/sample/Javaprogram.java
@@ -0,0 +1,28 @@
+package com.sample;
+
+import java.util.Scanner;
+
+public class Javaprogram {
+	    public static void main(String args[])
+	  {
+	    int first, second, add, subtract, multiply;
+	    float devide;
+	    Scanner scanner = new Scanner(System.in);
+
+	    System.out.print("Enter Two Numbers : ");
+	    first = scanner.nextInt();
+	    second = scanner.nextInt();
+
+	    add = first + second;
+	    subtract = first - second;
+	    multiply = first * second;
+	    devide = (float) first / second;
+
+	    System.out.println("Sum = " + add);
+	    System.out.println("Difference = " + subtract);
+	    System.out.println("Multiplication = " + multiply);
+	    System.out.println("Division = " + devide);
+	  }
+	}
+
+
